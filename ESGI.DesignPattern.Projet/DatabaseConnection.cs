bb0ff8b1af04d4b2307a8d8495fe7d7974164aa6
Bastien <nisole.bastien@outlook.fr>
2020-06-30 15:27:42 +0200

    Added singleton

---
+++ ESGI.DesignPattern.Projet/DatabaseConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+
+namespace ESGI.DesignPattern.Projet
+{
+    public static class DatabaseConnection
+    {
+        private static MySqlConnection databaseConnection;
+        private static readonly string DatabaseName = Environment.GetEnvironmentVariable("DATABASE_NAME");
+        private static readonly string User = Environment.GetEnvironmentVariable("DATABASE_USER");
+        private static readonly string Pass = Environment.GetEnvironmentVariable("DATABASE_PASSWORD");
+        
+        static DatabaseConnection()
+        {
+            using (databaseConnection = new MySqlConnection
+            {
+                ConnectionString = $"Database={DatabaseName};Data Source=localhost;User Id={User};Password={Pass}"
+            })
+            {
+                databaseConnection.Open();
+            }
+        }
+
+        public static MySqlConnection GetInstance()
+        {
+            return databaseConnection;
+        }
+    }
+}
